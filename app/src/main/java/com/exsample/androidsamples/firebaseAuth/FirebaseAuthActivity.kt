6db5002fe16245070b0cd56c3db7283c85c67553
yss.miura <jane.was.gentleman@gmail.com>
2020-02-08 15:47:53 +0900

    update Firebase Auth

--- app/src/main/java/com/exsample/androidsamples/firebaseAuth/FirebaseAuthActivity.kt
+++ app/src/main/java/com/exsample/androidsamples/firebaseAuth/FirebaseAuthActivity.kt
@@ -3,14 +3,24 @@ package com.exsample.androidsamples.firebaseAuth
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
+import android.util.Patterns
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.input.input
 import com.exsample.androidsamples.R
 import com.exsample.androidsamples.base.BaseActivity
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.firebase_auth_activity.*
+import timber.log.Timber
 import kotlin.random.Random
 
 class FirebaseAuthActivity: BaseActivity() {
+
+    private var loginRegisterType = LoginRegisterType.LOGIN
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,41 +41,159 @@ class FirebaseAuthActivity: BaseActivity() {
         closeImageView.setOnClickListener {
             finish()
         }
-        registerButton.setOnClickListener {
-            register()
+        loginTextView.setOnClickListener {
+            changeMenu(LoginRegisterType.LOGIN)
+        }
+        registerTextView.setOnClickListener {
+            changeMenu(LoginRegisterType.REGISTER)
+        }
+        forgetPasswordTextView.setOnClickListener {
+            changeMenu(LoginRegisterType.FORGET_MAIL)
+        }
+        executeButton.setOnClickListener {
+            execute()
         }
         logoutButton.setOnClickListener {
             logout()
+        }
+        changePasswordButton.setOnClickListener {
+            changePassword()
         }
     }
 
     private fun updateView() {
         var user = FirebaseAuth.getInstance().currentUser
         var isLogin = user != null
-        registerButton.visibility = if (isLogin) View.INVISIBLE else View.VISIBLE
-        logoutButton.visibility =  if (isLogin) View.VISIBLE else View.INVISIBLE
+        loginRegisterView.visibility = if (isLogin) View.INVISIBLE else View.VISIBLE
+        memberView.visibility =  if (isLogin) View.VISIBLE else View.INVISIBLE
         user?.also {
-            userTextView.text = "mail:${user.email}\nisAnonymous:${user.isAnonymous}"
+            myMailAddressTextView.text = "${user.email}"
+            uidTextView.text = "${user.uid}"
         } ?: run {
-            userTextView.text = ""
+            myMailAddressTextView.text = ""
+            uidTextView.text = ""
+        }
+        updateLoginRegisterMenuView()
+    }
+
+    private fun updateLoginRegisterMenuView() {
+        updateMenuTextView(loginTextView, loginRegisterType == LoginRegisterType.LOGIN)
+        updateMenuTextView(registerTextView, loginRegisterType == LoginRegisterType.REGISTER)
+        updateMenuTextView(forgetPasswordTextView, loginRegisterType == LoginRegisterType.FORGET_MAIL)
+        passwordView.visibility = if (loginRegisterType == LoginRegisterType.FORGET_MAIL) View.INVISIBLE else View.VISIBLE
+    }
+
+    private fun updateMenuTextView(textView: TextView, isSelected: Boolean) {
+        textView.apply {
+            setTextColor(ContextCompat.getColor(this@FirebaseAuthActivity, if (isSelected) android.R.color.white else android.R.color.black))
+            setBackgroundResource(if (isSelected) R.drawable.selector_button_black_to_black_80 else R.drawable.selector_button_white_to_white_80)
+        }
+    }
+
+    private fun changeMenu(newLoginRegisterType: LoginRegisterType) {
+        loginRegisterType = newLoginRegisterType
+        updateLoginRegisterMenuView()
+    }
+
+    private fun execute() {
+        when(loginRegisterType) {
+            LoginRegisterType.LOGIN -> login()
+            LoginRegisterType.REGISTER -> register()
+            LoginRegisterType.FORGET_MAIL -> forgetPassword()
+        }
+    }
+
+    private fun login() {
+        val pair = getPair()
+        pair?.also {
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(it.first, it.second)
+                .addOnCompleteListener {
+                    Timber.d("login task:$it")
+                    toast(if (it.isSuccessful) R.string.firebase_auth_success else R.string.firebase_auth_error)
+                    updateView()
+                }
         }
     }
 
     private fun register() {
-        var randomNum = Random.nextInt(1000000000)
-        FirebaseAuth.getInstance().createUserWithEmailAndPassword("jane-$randomNum@gmail.com", "$randomNum")
-            .addOnCompleteListener {
-               updateView()
-            }
+        val pair = getPair()
+        pair?.also {
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(it.first, it.second)
+                .addOnCompleteListener {
+                    Timber.d("register task:$it")
+                    toast(if (it.isSuccessful) R.string.firebase_auth_success else R.string.firebase_auth_error)
+                    updateView()
+                }
+        }
     }
 
-    private fun login() {
-        FirebaseAuth.getInstance().signInWithEmailAndPassword("めーる", "パスワード")
+    private fun forgetPassword() {
+        val pair = getPair(true)
+        pair?.also {
+            FirebaseAuth.getInstance().sendPasswordResetEmail(pair.first)
+                .addOnCompleteListener {
+                    Timber.d("forgetPassword task:$it")
+                    toast(if (it.isSuccessful) R.string.firebase_auth_success else R.string.firebase_auth_error)
+                }
+        }
     }
+
+    private fun getPair(isOnlyMailAddress: Boolean = false): Pair<String, String>? {
+        val mailAddress = getMailAddress()
+        if (mailAddress.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(mailAddress).matches()) {
+            toast(R.string.firebase_auth_warn_mail)
+            return null
+        }
+        val password = getPassword()
+        if (!isOnlyMailAddress && (password.isEmpty() || password.length < 8)){
+            toast(R.string.firebase_auth_warn_password)
+            return null
+        }
+        return Pair(mailAddress, password)
+    }
+
+    private fun getMailAddress(): String = mailEditText.text.toString()
+
+    private fun getPassword(): String = passwordEditText.text.toString()
 
     private fun logout() {
         FirebaseAuth.getInstance().signOut()
         updateView()
+    }
+
+    private fun changePassword() {
+        MaterialDialog(this).show {
+            title(R.string.firebase_auth_input_new_password)
+            input(inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD or InputType.TYPE_CLASS_TEXT) { _, text ->
+                changePassword(text.toString())
+            }
+            positiveButton(R.string.ok)
+            negativeButton(R.string.cancel)
+        }
+    }
+
+    private fun changePassword(password: String) {
+        if (password.isEmpty() || password.length < 8){
+            toast(R.string.firebase_auth_warn_password)
+            return
+        }
+        FirebaseAuth.getInstance().currentUser?.also {
+            it.updatePassword(password).addOnCompleteListener { task ->
+                Timber.d("changePassword task:$task")
+                toast(if (task.isSuccessful) R.string.firebase_auth_success else R.string.firebase_auth_error)
+            }
+        } ?: run {
+            toast(R.string.firebase_auth_warn_login)
+            updateView()
+        }
+    }
+
+    private fun toast(textId: Int) {
+        Toast.makeText(this, textId, Toast.LENGTH_SHORT).show()
+    }
+
+    enum class LoginRegisterType {
+        LOGIN, REGISTER, FORGET_MAIL
     }
 
     companion object {
